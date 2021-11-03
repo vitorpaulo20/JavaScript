f23230a440c79c7e58aa8a11c1ba291d204d0b42
Paulo Vitor <vitorpaulo17@hotmail.com>
2021-11-03 18:05:29 -0300

    minha primeira pagina com java script

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var nomeCriador="Paulo Vitor";
+var idadeCriador=23;
+var frase="Alemanha é topz";
+
+
+
+//alert("Sabemos que você possui: "+idadeCriador+" anos de idade");
+
+console.log(idadeCriador);
+console.log(nomeCriador);
+
+//sobre o replace dunciona também no Alert substituir palavra da string por outra palavra , não a string toda a palavra
+console.log(frase.replace("Alemanha","Brasil"));
+
+
+
+
