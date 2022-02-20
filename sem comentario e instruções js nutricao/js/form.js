436ce6d81598d53e991faafd1ae30ac63f02e9ba
Paulo Vitor <vitorpaulo17@hotmail.com>
2022-02-20 19:52:43 -0300

    versão final do projeto do curso nutricao sem comentarios

--- sem comentario e instruções js nutricao/js/form.js
+++ sem comentario e instruções js nutricao/js/form.js
@@ -6,8 +6,6 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var paciente = obtemPacienteDoFormulario(form);
 
-    var pacienteTr = montaTr(paciente);
-
     var erros = validaPaciente(paciente);
 
     if (erros.length > 0) {
@@ -16,9 +14,7 @@ botaoAdicionar.addEventListener("click", function(event) {
         return;
     }
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
 
@@ -101,4 +97,10 @@ function exibeMensagensDeErro(erros) {
         li.textContent = erro;
         ul.appendChild(li);
     });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 }
