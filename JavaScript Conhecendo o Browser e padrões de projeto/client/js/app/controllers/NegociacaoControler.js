b56054656d48b2457a08b452efab67cf3ad4cbc8
Paulo Vitor <vitorpaulo17@hotmail.com>
2022-02-25 22:22:39 -0300

    atualizações na classe controler

--- JavaScript Conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoControler.js
+++ JavaScript Conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoControler.js
@@ -1,10 +1,45 @@
-class NegociacaoControler{
+class NegociacaoControler {
 
+constructor(){
 
-adiciona(event){
-event.preventDefault();
-alert('Chamei o metodo da Negociação Controler');
+    let $ = document.querySelector.bind(document);
 
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+   
 }
+
+
+
+
+
+
+
+
+
+    adiciona(event) {
+
+
+        event.preventDefault();
+
+       // alert('Chamei o metodo da Negociação Controler');
+       
+       let dataTradada = this._inputData.value.split("-");
+      
+
+        let data = new Date(dataTradada);
+
+        console.log(data);
+    
+        // let negociacao = new Negociacao(
+        //     this._inputData.value,
+        //     this._inputQuantidade.value,
+        //     this._inputValor
+        // )
+
+       
+
+    }
 
 }
