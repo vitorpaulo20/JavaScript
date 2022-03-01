e1839f3658ac90aa16534efeaf6d1f455cd3c853
Paulo Vitor <vitorpaulo17@hotmail.com>
2022-03-01 16:56:51 -0300

    view models e controlers quase prontos

--- JavaScript Conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoControler.js
+++ JavaScript Conhecendo o Browser e padrões de projeto/client/js/app/controllers/NegociacaoControler.js
@@ -1,44 +1,43 @@
 class NegociacaoControler {
 
-constructor(){
+    constructor() {
 
-    let $ = document.querySelector.bind(document);
+        let $ = document.querySelector.bind(document);
 
-    this._inputData = $("#data");
-    this._inputQuantidade = $("#quantidade");
-    this._inputValor = $("#valor");
-   
-}
-
-
-
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
 
 
-
-
+    }
 
 
     adiciona(event) {
 
-
         event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
 
-       // alert('Chamei o metodo da Negociação Controler');
-       
-       let dataTradada = this._inputData.value.split("-");
-      
+    }
 
-        let data = new Date(dataTradada);
+    _criaNegociacao() {
 
-        console.log(data);
-    
-        // let negociacao = new Negociacao(
-        //     this._inputData.value,
-        //     this._inputQuantidade.value,
-        //     this._inputValor
-        // )
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
 
-       
+        this._inputData.focus();
 
     }
 
---
+++ JavaScript Conhecendo o Browser e padrões de projeto/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,18 @@
+class DateHelper {
+
+    constructor() {
+        throw new Error('DateHelper não pode ser instanciada');
+
+    }
+
+    static dataParaTexto(data) {
+
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+    }
+
+    static textoParaData(texto) {
+
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+
+    }
+}
